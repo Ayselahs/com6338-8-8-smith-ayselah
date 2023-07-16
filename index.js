984155619c5cdeb4f58ddd9c1d237bc064273fc4
Ayselah <ayselahs@gmail.com>
2023-07-15 22:13:31 -0500

    Fixed feels-like and current and formated code

--- index.js
+++ index.js
@@ -4,64 +4,69 @@ var form = document.querySelector('form')
 var cityInput = document.getElementById('weather-search')
 var section = document.getElementById('weather')
 
+
+
 form.onsubmit = function(e) {
     e.preventDefault()
     var city = cityInput.value.trim()
     var apiKey = '508e60c837e224615d876c902697bf3f'
     cityInput.value = ""
+    
   if (city !== '') {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`)
         .then(function(res) {
             return res.json()
         })
-        .then(function(weather) {
-            console.log(weather)
-            section.innerHTML = "" 
+        .then(function(weatherData) {
+            console.log(weatherData)
+            section.innerHTML = ""
+            
             var cityName = document.createElement('h2')
-            cityName.textContent = weather.name + ", " + weather.sys.country
+            cityName.textContent = weatherData.name + ", " + weatherData.sys.country
             section.appendChild(cityName)
 
+            // Map View
             var mapLink = document.createElement('a')
-            var latCoord = weather.coord.lat
-            var lonCoord = weather.coord.lon
+            var latCoord = weatherData.coord.lat
+            var lonCoord = weatherData.coord.lon
             mapLink.href = `https://www.google.com/maps/search/?api=1&query=${latCoord},${lonCoord}`
             mapLink.target = '_blank'
             mapLink.textContent = "Click to view map"
             section.appendChild(mapLink)
 
+            // City Image
             var cityImg = document.createElement('img')
-            var icon = weather.weather[0].icon
+            var icon = weatherData.weather[0].icon
             cityImg.src = `https://openweathermap.org/img/wn/${icon}@2x.png`
             section.appendChild(cityImg)
 
+            // Weather Condition
             var weatherCon = document.createElement('p')
-            weatherCon.textContent = weather.weather[0].description
+            weatherCon.textContent = weatherData.weather[0].description
             section.appendChild(weatherCon)
 
+            // Current Temp
             var current = document.createElement('p')
-            current.textContent = "Current: " + weather.main.feels_like + " F"
+            current.textContent = "Current: " + weatherData.main.temp + "°" + " F"
             section.appendChild(current)
 
+            // Temp Feels Like
             var feelsLike = document.createElement('p')
-            feelsLike.textContent = "Feels like: " + weather.main.temp + " F"
+            feelsLike.textContent = "Feels like: " + weatherData.main.feels_like + "°" + " F"
             section.appendChild(feelsLike)
 
+            // Time Weather
             var updated = document.createElement('p')
-            var time = new Date(weather.dt * 1000)
+            var time = new Date(weatherData.dt * 1000)
             var timeString = time.toLocaleTimeString('en-US', {
                 hour: 'numeric',
                 minute: '2-digit'
-            });
+            })
             updated.textContent = "Last updated: " + timeString
             section.appendChild(updated)
 
             
-
-                
-           
-
-
-       
+        
         })
         .catch(function(err) {
             section.innerHTML = ""
@@ -71,5 +76,5 @@ form.onsubmit = function(e) {
 
             console.error("Error fetching weather data:", err);
         })
-    } 
-}
+    }
+} 
